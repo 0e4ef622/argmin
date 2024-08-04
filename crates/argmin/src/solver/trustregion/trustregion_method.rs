5f82dd30d7b849d0ce6fee3e6f5a979f2b90bbe6
Matthew Tran <0e4ef622@gmail.com>
2024-08-04 11:06:15 -0500

    Fix clippy lints

--- crates/argmin/src/solver/trustregion/trustregion_method.rs
+++ crates/argmin/src/solver/trustregion/trustregion_method.rs
@@ -332,7 +332,7 @@ mod tests {
     #[test]
     fn test_with_radius() {
         // correct parameters
-        for radius in [std::f64::EPSILON, 1e-2, 1.0, 2.0, 10.0, 100.0] {
+        for radius in [f64::EPSILON, 1e-2, 1.0, 2.0, 10.0, 100.0] {
             let cp: CauchyPoint<f64> = CauchyPoint::new();
             let tr: TrustRegion<_, f64> = TrustRegion::new(cp);
             let res = tr.with_radius(radius);
@@ -360,10 +360,10 @@ mod tests {
         // correct parameters
         for eta in [
             0.0,
-            std::f64::EPSILON,
+            f64::EPSILON,
             1e-2,
             0.125,
-            0.25 - std::f64::EPSILON,
+            0.25 - f64::EPSILON,
         ] {
             let cp: CauchyPoint<f64> = CauchyPoint::new();
             let tr: TrustRegion<_, f64> = TrustRegion::new(cp);
